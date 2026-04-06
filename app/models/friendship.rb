bf11a4d1e99bdcdd070f58ba35c9cdc69c4ad091
rojanagunoori <nagunooriroja@gmail.com>
2026-04-06 15:54:32 +0530

    After bundal

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -7,8 +7,9 @@
   # Enum definition
   #enum friendship_status: { pending: 0, accepted: 1, rejected: 2 }, _prefix: true
   #enum status: { pending: 0, accepted: 1, rejected: 2 }, _prefix: true
-  #enum status: { pending: 0, accepted: 1, rejected: 2 }, _prefix: :status
-  enum :status, { pending: 0, accepted: 1, rejected: 2 }, prefix: :status
+  
+  #enum :status, { pending: 0, accepted: 1, rejected: 2 }, prefix: :status
+  enum status: { pending: 0, accepted: 1, rejected: 2 }, _prefix: :status
 
 
 end
