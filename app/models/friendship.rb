a55a2a02589ea65dc70ecf64ba64ebfbbdac5f2f
rojanagunoori <nagunooriroja@gmail.com>
2026-04-06 16:45:18 +0530

    Fix enum in Friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,12 +1,14 @@
 ﻿class Friendship < ApplicationRecord
-  # Associations
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
-  # Validations
   validates :user_id, uniqueness: { scope: :friend_id, message: "Friend request already sent" }
 
-  # Enum for friendship status
-  enum status: { pending: 0, accepted: 1, rejected: 2 }, _prefix: true
+  # Enum definition
+  #enum friendship_status: { pending: 0, accepted: 1, rejected: 2 }, _prefix: true
+  #enum status: { pending: 0, accepted: 1, rejected: 2 }, _prefix: true
   #enum status: { pending: 0, accepted: 1, rejected: 2 }, _prefix: :status
+  enum :status, { pending: 0, accepted: 1, rejected: 2 }, prefix: :status
+
+
 end
