06c546a15474adffc7ca6b1c626d0575a6b51463
rojanagunoori <nagunooriroja@gmail.com>
2026-04-06 12:28:28 +0530

    chnages

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,13 +1,11 @@
 ﻿//= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
 
+//= link_directory ../stylesheets .css
 
 //= link_tree ../../javascript .js
 
 //= link application.js
 //= link controllers/application.js
-
 
 //= link rails-ujs.js
 
