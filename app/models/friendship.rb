f4f3896a3a9a7a53cb7545c4be46a67f6c9438c6
rojanagunoori <nagunooriroja@gmail.com>
2026-04-06 16:04:52 +0530

    After bundal

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,6 +4,6 @@
 
   validates :user_id, uniqueness: { scope: :friend_id, message: "Friend request already sent" }
 
-  # Correct enum declaration in Rails 8
+  # Correct enum declaration
   enum status: { pending: 0, accepted: 1, rejected: 2 }, _prefix: :status
 end
